5c7323959a0d3183d999753f146ac7872d3fc15a
chaobinwu <chaobinwu@tagged.com>
2012-09-05 16:07:24 +0800

    init project of qq demo.

---
+++ src/com/aven/qqdemo/MainActivity.java
@@ -0,0 +1,232 @@
+package com.aven.qqdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.demo.R;
+
+import android.R.integer;
+import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * 参考原作者D.Winter基础，
+ * 
+ * @author D.Winter
+ * 
+ */
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    private ViewPager mPager;
+    private List<View> listViews;
+    private ImageView ivBottomLine;
+    private TextView tvTabActivity, tvTabGroups, tvTabFriends, tvTabChat;
+
+    private int currIndex = 0;
+    private int bottomLineWidth;
+    private int offset = 0;
+    private int position_one;
+    private int position_two;
+    private int position_three;
+    private Resources resources;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main);
+        resources = getResources();
+        InitWidth();
+        InitTextView();
+        InitViewPager();
+    }
+
+    private void InitTextView() {
+        tvTabActivity = (TextView) findViewById(R.id.tv_tab_activity);
+        tvTabGroups = (TextView) findViewById(R.id.tv_tab_groups);
+        tvTabFriends = (TextView) findViewById(R.id.tv_tab_friends);
+        tvTabChat = (TextView) findViewById(R.id.tv_tab_chat);
+
+        tvTabActivity.setOnClickListener(new MyOnClickListener(0));
+        tvTabGroups.setOnClickListener(new MyOnClickListener(1));
+        tvTabFriends.setOnClickListener(new MyOnClickListener(2));
+        tvTabChat.setOnClickListener(new MyOnClickListener(3));
+    }
+
+    private void InitViewPager() {
+        mPager = (ViewPager) findViewById(R.id.vPager);
+        listViews = new ArrayList<View>();
+        LayoutInflater mInflater = getLayoutInflater();
+        listViews.add(mInflater.inflate(R.layout.lay1, null));
+        listViews.add(mInflater.inflate(R.layout.lay2, null));
+        listViews.add(mInflater.inflate(R.layout.lay3, null));
+        listViews.add(mInflater.inflate(R.layout.lay1, null));
+        mPager.setAdapter(new MyPagerAdapter(listViews));
+        mPager.setCurrentItem(0);
+        mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+    }
+
+    private void InitWidth() {
+        ivBottomLine = (ImageView) findViewById(R.id.iv_bottom_line);
+        bottomLineWidth = ivBottomLine.getLayoutParams().width;
+        Log.d(TAG, "cursor imageview width=" + bottomLineWidth);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenW = dm.widthPixels;
+        offset = (int) ((screenW / 4.0 - bottomLineWidth) / 2);
+        Log.i("MainActivity", "offset=" + offset);
+
+        position_one = (int) (screenW / 4.0);
+        position_two = position_one * 2;
+        position_three = position_one * 3;
+    }
+
+    public class MyPagerAdapter extends PagerAdapter {
+        public List<View> mListViews;
+
+        public MyPagerAdapter(List<View> mListViews) {
+            this.mListViews = mListViews;
+        }
+
+        @Override
+        public void destroyItem(View arg0, int arg1, Object arg2) {
+            ((ViewPager) arg0).removeView(mListViews.get(arg1));
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+        }
+
+        @Override
+        public int getCount() {
+            return mListViews.size();
+        }
+
+        @Override
+        public Object instantiateItem(View arg0, int arg1) {
+            ((ViewPager) arg0).addView(mListViews.get(arg1), 0);
+            return mListViews.get(arg1);
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == (arg1);
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {
+        }
+    }
+
+    public class MyOnClickListener implements View.OnClickListener {
+        private int index = 0;
+
+        public MyOnClickListener(int i) {
+            index = i;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mPager.setCurrentItem(index);
+        }
+    };
+
+    public class MyOnPageChangeListener implements OnPageChangeListener {
+
+        @Override
+        public void onPageSelected(int arg0) {
+            Animation animation = null;
+            switch (arg0) {
+            case 0:
+                if (currIndex == 1) {
+                    animation = new TranslateAnimation(position_one, 0, 0, 0);
+                    tvTabGroups.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 2) {
+                    animation = new TranslateAnimation(position_two, 0, 0, 0);
+                    tvTabFriends.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 3) {
+                    animation = new TranslateAnimation(position_three, 0, 0, 0);
+                    tvTabChat.setTextColor(resources.getColor(R.color.lightwhite));
+                }
+                tvTabActivity.setTextColor(resources.getColor(R.color.white));
+                break;
+            case 1:
+                if (currIndex == 0) {
+                    animation = new TranslateAnimation(offset, position_one, 0, 0);
+                    tvTabActivity.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 2) {
+                    animation = new TranslateAnimation(position_two, position_one, 0, 0);
+                    tvTabFriends.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 3) {
+                    animation = new TranslateAnimation(position_three, position_one, 0, 0);
+                    tvTabChat.setTextColor(resources.getColor(R.color.lightwhite));
+                }
+                tvTabGroups.setTextColor(resources.getColor(R.color.white));
+                break;
+            case 2:
+                if (currIndex == 0) {
+                    animation = new TranslateAnimation(offset, position_two, 0, 0);
+                    tvTabActivity.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 1) {
+                    animation = new TranslateAnimation(position_one, position_two, 0, 0);
+                    tvTabGroups.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 3) {
+                    animation = new TranslateAnimation(position_three, position_two, 0, 0);
+                    tvTabChat.setTextColor(resources.getColor(R.color.lightwhite));
+                }
+                tvTabFriends.setTextColor(resources.getColor(R.color.white));
+                break;
+            case 3:
+                if (currIndex == 0) {
+                    animation = new TranslateAnimation(offset, position_three, 0, 0);
+                    tvTabActivity.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 1) {
+                    animation = new TranslateAnimation(position_one, position_three, 0, 0);
+                    tvTabGroups.setTextColor(resources.getColor(R.color.lightwhite));
+                } else if (currIndex == 2) {
+                    animation = new TranslateAnimation(position_two, position_three, 0, 0);
+                    tvTabFriends.setTextColor(resources.getColor(R.color.lightwhite));
+                }
+                tvTabChat.setTextColor(resources.getColor(R.color.white));
+                break;
+            }
+            currIndex = arg0;
+            animation.setFillAfter(true);
+            animation.setDuration(300);
+            ivBottomLine.startAnimation(animation);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    }
+}
